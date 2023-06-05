ed8406e30ac3506169e07d136d44aa7ef8d0c5a5
BintangAldian17 <bintangaldian17@gmail.com>
2023-06-06 01:32:46 +0700

    resolve button ui

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,7 @@ import resume from "../assets/Muhammad_Bintang Aldiansyah_Resume_.pdf";
 const SideBar = ({ open, hero, contact, portofolio, about, scrollSection, handleClose }) => {
   return (
     <div
-      className={`absolute left-0 -z-[1] border-b-4 border-violet-700 bg-[#f2f2f2] w-full flex justify-center items-center px-3 ${
+      className={`absolute left-0 -z-[1] border-b-4 border-gray-500  bg-[#f2f2f2] w-full flex justify-center items-center px-3 ${
         open ? "top-0 h-[80vh]" : " invisible"
       }`}>
       <div className=" w-full h-[70%] shadow-neutral-950 shadow-[4px_6px_0_0] border-2 border-neutral-950">
@@ -40,11 +40,11 @@ const SideBar = ({ open, hero, contact, portofolio, about, scrollSection, handle
             }}>
             <h1 className=" font-bold text-3xl">Contact</h1>
           </div>
-          <div className=" w-full flex items-center justify-center">
+          <div className=" w-full h-fit flex items-center justify-center">
             <a
               className={`${
                 open ? "visible" : "invisible"
-              } w-36 h-10 border border-black bg-violet-600 shadow-[3px_4px__1px_1px] shadow-black hover:translate-y-1 hover:translate-x-[1px] hover:bg-violet-500 hover:shadow-[1px_1px_0_0] justify-center items-center text-neutral-900 font-bold cursor-pointer`}
+              }  px-9 py-[4px] border border-black bg-violet-600 shadow-[3px_4px__1px_1px] shadow-black hover:translate-y-1 hover:translate-x-[1px] hover:bg-violet-500 hover:shadow-[1px_1px_0_0]  text-neutral-900 font-bold cursor-pointer flex`}
               href={resume}
               download="Resume_Bintang_Aldian">
               Hire me!
